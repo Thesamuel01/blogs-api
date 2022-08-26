be1e02470ec4d19d0a58c431bce7c3b8bbfa11d4
samuel santos <samaraujo28@gmail.com>
2022-08-26 18:09:42 -0300

    feat: implement create-postcategories migration

--- src/database/migrations/20220826210051-create-postcategories.js
+++ src/database/migrations/20220826210051-create-postcategories.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        field: 'post_id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        field: 'category_id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('PostCategories');
   }
 };
