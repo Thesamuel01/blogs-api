59ba9884f7d2fa737f24c5fd72f3f77ce8dbbdaa
samuel santos <samaraujo28@gmail.com>
2022-08-26 18:37:28 -0300

    feat: implement User model and its association

---
+++ src/database/models/User.js
@@ -0,0 +1,19 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogposts'});
+  };
+
+  return User;
+};
