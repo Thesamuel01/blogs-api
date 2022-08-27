2c79d3bbe3e8be9c5f049304829300c9294cb8c6
samuel santos <samaraujo28@gmail.com>
2022-08-27 01:16:52 -0300

    fix: remove message method

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,15 +1,10 @@
 const joi = require('joi');
 
 const schema = joi.object({
-  displayName: joi.string().min(8).required().message({
-    message: '400|"displayName" length must be at least 8 characters long',
-  }),
-  email: joi.string().email().required().message({
-    message: '400|"email" must be a valid email',
-  }),
-  password: joi.string().min(6).required().message({
-    message: '400|"password" length must be at least 6 characters long',
-  }),
+  displayName: joi.string().min(8).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+  image: joi.string(),
 });
 
 module.exports = (login) => schema.validate(login);
