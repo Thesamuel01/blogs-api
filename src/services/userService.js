8bf11391ca367dfc2251b7f741f481e07118710a
samuel santos <samaraujo28@gmail.com>
2022-08-27 00:48:48 -0300

    feat: implement createUser function in userService file

---
+++ src/services/userService.js
@@ -0,0 +1,43 @@
+const boom = require('@hapi/boom');
+
+const { User, sequelize } = require('../database/models');
+const tokenUtilities = require('../utils/token');
+
+const checkEmailRegister = async (email) => {
+  const result = await User.findOne({
+    where: { email },
+  });
+
+  return result !== null;
+};
+
+const createUser = async ({ displayName, email, password, image }) => {
+  const hasUser = await checkEmailRegister(email);
+
+  if (hasUser) throw boom.conflict('User already registered');
+
+  let transaction;
+  let dbError = null;
+
+  try {
+    transaction = await sequelize.transaction();
+
+    await User.create({ displayName, email, password, image }, { transaction });
+
+    await transaction.commit();
+  } catch (error) {
+    dbError = error;
+
+    await transaction.rollback();
+  }
+
+  if (dbError) throw new Error(dbError.message);
+
+  const token = tokenUtilities.createToken({ email });
+
+  return token;
+};
+
+module.exports = {
+  createUser,
+};
