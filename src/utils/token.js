b90ec90aace4cfb4913f05a4bdcc4218cd75eadf
samuel santos <samaraujo28@gmail.com>
2022-08-27 02:39:08 -0300

    feat: implement validate token function

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,3 +1,4 @@
+const boom = require('@hapi/boom');
 const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
@@ -5,10 +6,21 @@ const JWT_OPTIONS = { algorithm: 'HS256', expiresIn: '1d' };
 
 const createToken = (payload) => {
   const token = jwt.sign(payload, JWT_SECRET, JWT_OPTIONS);
-
+  console.log(token);
   return token;
+};
+
+const validateToken = (token) => {
+  jwt.verify(token, JWT_SECRET, (err, decode) => {
+    if(err) {
+      throw boom.unauthorized('Expired or invalid token');
+    }
+
+    return decode;
+  });
 };
 
 module.exports = {
   createToken,
+  validateToken,
 };
