8535921e815f42ee5ac3118bfe64527ba801b580
samuel santos <samaraujo28@gmail.com>
2022-08-26 23:09:05 -0300

    fix: change way payload is pass for jwt func and comment User associate until implement BLogPost model

--- src/utils/token.js
+++ src/utils/token.js
@@ -4,7 +4,7 @@ const { JWT_SECRET } = process.env;
 const JWT_OPTIONS = { algorithm: 'HS256', expiresIn: '1d' };
 
 const createToken = (payload) => {
-  const token = jwt.sign(payload, JWT_SECRET, JWT_OPTIONS);
+  const token = jwt.sign({ payload }, JWT_SECRET, JWT_OPTIONS);
 
   return token;
 };
