c4dc9234292ad57733389a150c391f840ae11027
samuel santos <samaraujo28@gmail.com>
2022-08-26 23:06:47 -0300

    feat: add loginController in its route and add login route into index.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,11 +2,13 @@ const express = require('express');
 const rescue = require('express-rescue');
 
 const middlewares = require('../middlewares');
+const loginController = require('../controllers/loginController');
 
 const loginRoute = express.Router();
 
 loginRoute.post('/', [
   rescue(middlewares.loginValidation),
+  rescue(loginController.login),
 ]);
 
 module.exports = loginRoute;
