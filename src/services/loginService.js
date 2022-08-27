f8791f02370473bbe755f8590bcbc7ea9ef27a6f
samuel santos <samaraujo28@gmail.com>
2022-08-26 22:31:07 -0300

    feat: implement getToken function in login service

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,1 +1,23 @@
+const boom = require('@hapi/boom');
 
+const { User } = require('../database/models/');
+const tokens = require('../utils/token');
+
+const getToken = async ({ email, password}) => {
+  const result = await User.findOne({
+    where: {
+      email,
+      password,
+    },
+  });
+
+  if(!result) throw boom.badRequest('Invalid fields');
+
+  const token = tokens.createToken(email);
+
+  return res.status(200).json({ token });
+};
+
+module.exports = {
+  getToken,
+};
