431d886e733270b573d4b36b667fa623f32a3ee4
samuel santos <samaraujo28@gmail.com>
2022-08-30 00:59:10 -0300

    feat: implement get all categories controller

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -8,6 +8,13 @@ const create = async (req, res) => {
   return res.status(201).json(result);
 };
 
+const getAll = async (_req, res) => {
+  const categories = await categoriesService.getAllCategories();
+
+  return res.status(200).json(categories);
+};
+
 module.exports = {
   create,
+  getAll,
 };
