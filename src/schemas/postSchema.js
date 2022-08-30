c5329649ede9d080fe06a5b50eaffa4d031cb985
samuel santos <samaraujo28@gmail.com>
2022-08-30 23:35:22 +0000

    feat: implement postt cschema validation

---
+++ src/schemas/postSchema.js
@@ -0,0 +1,11 @@
+const joi = require('joi');
+
+const schema = joi.object({
+  title: joi.string().required(),
+  content: joi.string().required(),
+  categoryIds: joi.array().items(
+    joi.number().required(),
+  ).required(),
+});
+
+module.exports = (body) => schema.validate(body);
