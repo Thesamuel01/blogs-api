bff99cb826ba3483f4455094ebcbedad234afb69
samuel santos <samaraujo28@gmail.com>
2022-08-30 00:05:14 -0300

    feat: implement categories validation middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,10 +2,12 @@ const errorHandler = require('./errorHandler');
 const loginValidation = require('./loginValidation');
 const userValidation = require('./userValidation');
 const auth = require('./authorization');
+const categoryValidation = require('./categoriesValidation');
 
 module.exports = {
   errorHandler,
   loginValidation,
   userValidation,
   auth,
+  categoryValidation,
 };
