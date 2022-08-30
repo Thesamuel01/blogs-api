d4235243d0fe25920d198a7000bb34cc61d76c30
samuel santos <samaraujo28@gmail.com>
2022-08-30 00:02:55 -0300

    feat: implement create category function in categories service

---
+++ src/services/categoriesService.js
@@ -0,0 +1,27 @@
+const { Category, sequelize } = require('../database/models');
+
+const createCategory = async (name) => {
+  let category;
+  let transaction;
+  let dbError = null;
+
+  try {
+    transaction = await sequelize.transaction();
+
+    category = await Category.create({ name }, { transaction });
+
+    await transaction.commit();
+  } catch (error) {
+    dbError = error;
+
+    await transaction.rollback();
+  }
+
+  if (dbError) throw new Error(dbError.message);
+
+  return category;
+};
+
+module.exports = {
+  createCategory,
+};
