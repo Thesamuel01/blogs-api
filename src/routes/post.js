dc5d0acae66546ec7f6d7aff9e335ef9feee5e5b
samuel santos <samaraujo28@gmail.com>
2022-08-31 00:25:11 +0000

    feat: implement /post get route

--- src/routes/post.js
+++ src/routes/post.js
@@ -6,6 +6,11 @@ const postController = require('../controllers/postController');
 
 const postRoute = express.Router();
 
+postRoute.get('/', [
+  rescue(middlewares.auth),
+  rescue(postController.getAll),
+]);
+
 postRoute.post('/', [
   rescue(middlewares.auth),
   rescue(middlewares.postValidation),
