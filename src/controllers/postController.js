47b3126f7193b8381a7019c45c007de27dbe5e44
samuel santos <samaraujo28@gmail.com>
2022-08-31 00:45:37 +0000

    feat: implement /post/:id route controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -15,7 +15,15 @@ const getAll = async (_req, res) => {
   return res.status(200).json(posts);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const post = await postService.getPost(id);
+
+  return res.status(200).json(post);
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
